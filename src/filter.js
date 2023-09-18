12e5bc91c8ccb2cd0dcc48a5c418ba6b11a59b87
Evgenia0712 <e.gunko1992@mail.ru>
2023-09-18 23:23:46 +0300

    change radio-button

--- src/filter.js
+++ src/filter.js
@@ -308,3 +308,36 @@ function filtermobile() {
 document.getElementById("btnappfilter1").addEventListener("click", () => {
 	filtermobile();
 });
+
+/* ____________FILTER__________ */
+const inputContainer = document.getElementById('inputContainer');
+const select = document.getElementById('pet');
+const form = document.forms.formFilter;
+const buttonSearch = document.getElementById('btnFilter');
+const buttonReset = document.getElementById('btnReboot');
+
+function showOther() {
+	if (select.value === "other") {
+		inputContainer.style.display = 'block';
+	} else {
+		inputContainer.style.display = 'none';
+	}
+}
+
+buttonReset.addEventListener('click', function () {
+	let inputElements = document.querySelectorAll('input');
+	select.value = "choose";
+	inputContainer.style.display = 'none';
+	inputElements.forEach(function (input) {
+		input.value = input.defaultValue;
+	});
+})
+
+buttonSearch.addEventListener('click', function () {
+	let inputElements = document.querySelectorAll('input');
+	select.value = "choose";
+	inputContainer.style.display = 'none';
+	inputElements.forEach(function (input) {
+		input.value = input.defaultValue;
+	});
+})
